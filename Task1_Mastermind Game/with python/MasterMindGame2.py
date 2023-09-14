b05d7cdd477318ac9666d577ea6a9b5ac43b463a
moses-irudayaraj <143519523+moses-irudayaraj@users.noreply.github.com>
2023-09-14 21:19:00 +0530

    Updated MasterMindGame2.py

--- Task1_Mastermind Game/with python/MasterMindGame2.py
+++ Task1_Mastermind Game/with python/MasterMindGame2.py
@@ -1,53 +1,69 @@
 import random
-player = random.randint(1,10)
 
-pl_ts=0
-opp_ts=0
-if(player%2==0):
-    opponent=1
-    player =1
+# Generate a random player number between 1 and 10
+player = random.randint(1, 10)
+
+# Initialize scores for players
+pl_ts = 0
+opp_ts = 0
+
+# Determine the opponent based on the initially chosen player
+if player % 2 == 0:
+    opponent = 1
+    player = 1
 else:
-    opponent=2
-    player=1
-pl_score,opp_score = 0,0
-players = {player:pl_score, opponent:opp_score}
+    opponent = 2
+    player = 1
 
+# Initialize scores for both players using a dictionary
+pl_score, opp_score = 0, 0
+players = {player: pl_score, opponent: opp_score}
+
+# Iterate through players and their turns
 for curr in players.keys():
-    isTrue=True
+    isTrue = True
     pl_num = int(input(f"\n\nPlayer{curr} please enter a Multi-Digit Number: "))
     pl_num = str(pl_num)
-    if(curr%2==0):
-        opponent=1
+
+    # Determine the opponent for the current player
+    if curr % 2 == 0:
+        opponent = 1
     else:
-        opponent=2
-    while(isTrue):
+        opponent = 2
+
+    while isTrue:
         print(f"\n Player{opponent} guess the number. It is a {len(pl_num)} digit number \n")
         opp_num = int(input("\n Enter your guess: "))
         opp_num = str(opp_num)
-        g_p=0
-        pl_ts+=1
+        g_p = 0
+        pl_ts += 1
+
+        # Compare digits and provide feedback
         for i in range(len(pl_num)):
-            if (pl_num[i] == opp_num[i]):
-                print(opp_num[i],end="\t")
-                g_p +=1;
+            if pl_num[i] == opp_num[i]:
+                print(opp_num[i], end="\t")
+                g_p += 1
             else:
                 print('_', end='\t')
-        if(g_p==len(pl_num)):
+
+        if g_p == len(pl_num):
             print('\n\n Excellent you guessed right!')
-            if(pl_ts == 1):
-                print(f"\n Congrats Mastermind! player{curr} you won in first attempt itself!!!")
-                exit(0);
-            isTrue=False
+
+            # Check if the player won in the first attempt
+            if pl_ts == 1:
+                print(f"\n Congrats Mastermind! player{curr} you won in the first attempt itself!!!")
+                exit(0)
+            isTrue = False
         else:
             print('\n\n try again')
-        
-    print("The number of times you tried: ",pl_ts)
-    players[curr]=pl_ts
-    pl_ts=0
 
+    # Display the number of attempts the player took
+    print("The number of times you tried: ", pl_ts)
+    players[curr] = pl_ts
+    pl_ts = 0
 
-if(opp_ts<pl_ts):
+# Determine the winner based on the scores
+if opp_ts < pl_ts:
     print(f"\n Congrats Mastermind! player{opponent} you won")
 else:
     print(f"\n Congrats Mastermind! player{player} you won")
-
